93a042a90645cbf6f26f1cc113e81f48560b7804
w4bo <m.francia@unibo.it>
2024-02-08 16:54:16 +0100

    fix download and add gitignore

--- code/download.sh
+++ code/download.sh
@@ -4,17 +4,17 @@ cd mongodb/data
 if test -f "games.bson"; then
     echo "games.bson exists"
 else
-    curl -o games.bson http://big.csr.unibo.it/projects/nosql-datasets/games.bson
+    curl --fail -L -o games.bson http://big.csr.unibo.it/projects/nosql-datasets/games.bson
 fi
 if test -f "restaurants.bson"; then
     echo "restaurants.bson exists"
 else
-    curl -o restaurants.bson http://big.csr.unibo.it/projects/nosql-datasets/restaurants.bson
+    curl --fail -L -o restaurants.bson http://big.csr.unibo.it/projects/nosql-datasets/restaurants.bson
 fi
 if test -f "yelp-business.bson"; then
     echo "yelp-business.bson exists"
 else
-    curl -o yelp-business.bson http://big.csr.unibo.it/projects/nosql-datasets/yelp-business.bson
+    curl --fail -L -o yelp-business.bson http://big.csr.unibo.it/projects/nosql-datasets/yelp-business.bson
 fi
 ls -las
 cd -
