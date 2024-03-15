d19b764d1d2766361560d68d0ec91db018070c63
Matteo Francia <m.francia@unibo.it>
2024-03-15 10:38:44 +0100

    Update download.bat

--- code/download.bat
+++ code/download.bat
@@ -2,18 +2,18 @@ cd mongodb\data
 IF EXIST "games.bson" (
   echo games.bson exists
 ) ELSE (
-  curl -o games.bson https://big.csr.unibo.it/projects/nosql-datasets/games.bson
+  curl --fail -L -o  games.bson https://big.csr.unibo.it/projects/nosql-datasets/games.bson
 )
 
 IF EXIST "restaurants.bson" (
   echo restaurants.bson exists
 ) ELSE (
-  curl -o restaurants.bson https://big.csr.unibo.it/projects/nosql-datasets/restaurants.bson
+  curl --fail -L -o  restaurants.bson https://big.csr.unibo.it/projects/nosql-datasets/restaurants.bson
 )
 
 IF EXIST "yelp-business.bson" (
   echo yelp-business.bson exists
 ) ELSE (
-  curl -o yelp-business.bson https://big.csr.unibo.it/projects/nosql-datasets/yelp-business.bson
+  curl --fail -L -o  yelp-business.bson https://big.csr.unibo.it/projects/nosql-datasets/yelp-business.bson
 )
 cd ..\..
